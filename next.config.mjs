c5f5e4754a3dd110e58e753def02b6243cc3fe47
d1maash <dimashaijarikov@gmail.com>
2025-11-17 12:22:13 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["*"]
-    }
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
 
